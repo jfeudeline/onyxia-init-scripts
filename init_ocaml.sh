2ff0261590fd32e6fc87ed85c6ce05f7edad79b4
Jean-Francois EUDELINE <jfeudeline@gmail.com>
2024-03-05 16:20:51 +0100

    Update init_ocaml.sh

--- init_ocaml.sh
+++ init_ocaml.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install gcc 
-sudo apt-get install build-essential 
-sudo apt-get install curl
-sudo apt-get installunzip
-sudo apt-get install bubblewrap
+sudo apt-get install opam
 
-sudo bash -c "sh <(curl -fsSL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)"
