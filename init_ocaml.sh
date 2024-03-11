41c43d8b5abea9d6755b458148f1f8949ee726e1
Jean-Francois EUDELINE <jfeudeline@gmail.com>
2024-03-11 16:05:30 +0100

    Update init_ocaml.sh

--- init_ocaml.sh
+++ init_ocaml.sh
@@ -6,3 +6,4 @@ apt-get install -y opam
 opam init --disable-sandboxing -y
 opam install -y ocaml-lsp-server odoc ocamlformat utop
 eval $(opam env)
+code --install-extension ocamllabs.ocaml-platform
