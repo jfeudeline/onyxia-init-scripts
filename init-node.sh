41482cd77e1854bd8c8028ef2d1473d7b2595b33
Jean-Francois EUDELINE <jfeudeline@gmail.com>
2024-03-11 16:41:19 +0100

    Update init-node.sh

--- init-node.sh
+++ init-node.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#!/bin/bash
+
+# On enregistre tous les logs dans log.out pour pouvoir déboguer
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>log.out 2>&1
+
+# Pareil pour les variables d'environnement
+env | sort > env_init.out
+
+# Et pour le script d'initialisation utilisé
+wget -O init_originel.sh ${PERSONAL_INIT_SCRIPT}
+
+
 apt-get update
 apt-get install -y ca-certificates curl gnupg
 mkdir -p /etc/apt/keyrings
